04777689e07995b0877e2f9f86574b6c9871732a
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2024-11-06 10:46:56 +0100

    🚧 Work in progress - Header has been created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <header>
-        <h1 className="logo">JS Monorepo</h1>
-      </header>
+      <Header />
 
       <nav className="navbar">
         <ul>
